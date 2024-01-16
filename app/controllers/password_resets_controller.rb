585f23f1ea1801e37e00be4f72f35f28610ede0d
Juraj Matase <juraj@matase.sk>
2024-01-16 18:40:18 +0100

    Fixing bug in PasswordResetsController

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,7 +7,7 @@ class PasswordResetsController < ApplicationController
     if (user = User.find_by(email: params[:email]))
       PasswordMailer.with(
         user: user,
-        tolen: user.generate_token_for(:password_reset)
+        token: user.generate_token_for(:password_reset)
       ).password_reset.deliver_later
     end
 
@@ -30,5 +30,9 @@ class PasswordResetsController < ApplicationController
   def set_user_by_token
     @user = User.find_by_token_for(:password_reset, params[:token])
     redirect_to new_password_reset_path alert: "Invalid token, please try again." unless @user.present?
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
