24c2f32433bbd0558a9a1cad1584e60f55b94905
Juraj Matase <juraj@matase.sk>
2024-02-05 19:00:37 +0100

    Adding Post model with Trix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   has_secure_password
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   normalizes :email, with: ->(email) {email.strip.downcase}
