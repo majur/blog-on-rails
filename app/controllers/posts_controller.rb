b311c1a0d51a27b6eb6f1c5252af6f153ada9daa
Juraj Matase <juraj@matase.sk>
2024-02-06 23:31:53 +0100

    Adding simplecss

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,59 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_author, only: [:edit, :update, :index, :show, :new, :create]
 
-    def index
-      @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def new
-      @post = Post.new
-    end
+  def new
+    @post = Post.new
+  end
 
-    def create
-      @post = current_user.posts.new(post_params)
+  def create
+    @post = current_user.posts.new(post_params)
 
-      if @post.save
-        redirect_to @post, notice: 'Post was successfully created.'
-      else
-        flash[:alert] = "Title can't be blank."
-        render :new
-      end
-    end
-
-    def edit
-    end
-
-    def update
-      if @post.update(post_params)
-        redirect_to @post, notice: 'Post was successfully updated.'
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @post.destroy
-      redirect_to posts_url, notice: 'Post was successfully destroyed.'
-    end
-
-    private
-
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    def post_params
-      params.require(:post).permit(:title, :content, :user_id)
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      flash[:alert] = "Title can't be blank."
+      render :new
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :user_id)
+  end
+
+  def authorize_author
+    unless current_user && current_user.author?
+      redirect_to root_path, alert: 'You are not authorized to access this page.'
+    end
+  end
+end
