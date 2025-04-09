0ff714dcc84538153f942335506cc3ae78d42715
Juraj Matase <jurajmatase@Mac-mini-uzivatela-Juraj.local>
2025-04-09 13:01:42 +0200

    Fixing webpacker on github

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,21 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+# Mock Webpacker pre testovacie prostredie
+if defined?(Webpacker::Manifest)
+  module WebpackerTestSupport
+    def lookup_pack_with_chunks(name, type: nil)
+      []
+    end
+
+    def lookup_pack_path(name, **options)
+      "mock/path/to/#{name}"
+    end
+  end
+
+  Webpacker::Manifest.prepend(WebpackerTestSupport)
+end
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
